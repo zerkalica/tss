c68bed1e5e74ea4831daaccc95be2d4cc5076220
Stefan Zerkalica <zerkalica@gmail.com>
2019-05-20 01:23:14 +0300

    feat: pereferials draft

---
+++ pereferials/pereferials.view.ts
@@ -0,0 +1,124 @@
+namespace $.$$ {
+	export class $mpk_tss_pereferials extends $.$mpk_tss_pereferials {
+		@ $mol_mem
+		units() {
+			return [
+				{
+					id: '1',
+					type: 'UPS',
+					element: 'Shtil SR1103L N1',
+					status: 'error',
+					paremeters: [
+						{
+							name: 'State',
+							value: 'notresponding',
+							update: new Date().toString(),
+						},
+						{
+							name: 'Power source',
+							value: null,
+							update: null,
+						},
+						{
+							name: 'Voltage',
+							value: null,
+							update: null,
+						},
+						{
+							name: 'Bat. level',
+							value: null,
+							update: null,
+						},
+						{
+							name: 'Bat. time',
+							value: null,
+							update: null,
+						},
+					],
+				},
+				{
+					id: '2',
+					type: 'UPS',
+					element: 'Shtil SR1103L N2',
+					status: 'ready',
+					paremeters: [
+						{
+							name: 'State',
+							value: 'ready',
+							update: new Date().toString(),
+						},
+						{
+							name: 'Power source',
+							value: 'AC',
+							update: new Date().toString(),
+						},
+						{
+							name: 'Voltage',
+							value: 220,
+							update: new Date().toString(),
+						},
+						{
+							name: 'Bat. level',
+							value: 33,
+							update: new Date().toString(),
+						},
+						{
+							name: 'Bat. time',
+							value: 134,
+							update: new Date().toString(),
+						},
+					],
+				},
+				{
+					id: '3',
+					type: 'AVR',
+					element: 'AVR Electricity system N1',
+					status: 'ready',
+					paremeters: [
+						{
+							name: 'State',
+							value: 'ready',
+							update: new Date().toString(),
+						},
+						{
+							name: 'Voltage',
+							value: 12,
+							update: new Date().toString(),
+						},
+					],
+				},
+			]
+		}
+
+		unit_cards() {
+			return this.units().map((unit, index) => this.Unit_card(index))
+		}
+		unit(index : any ) {
+			return this.units()[index]
+		}
+		unit_id(index: any) {
+			return this.units()[index].id
+		}
+		
+	}
+
+	export class $mpk_tss_pereferials_unit_card extends $.$mpk_tss_pereferials_unit_card {
+		
+		unit() {
+			return null as any
+		}
+
+		type() {
+			return this.unit().type
+		}
+
+		element() {
+			return this.unit().element
+		}
+
+		status() {
+			return this.unit().status
+		}
+	}
+	
+}
