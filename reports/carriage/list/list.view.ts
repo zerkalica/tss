b2e2b4c7c5e2ed8a9ea09a65552856f9fa2daf2e
Stefan Zerkalica <zerkalica@gmail.com>
2019-05-29 14:01:57 +0300

    refactor: carriage list

--- reports/carriage/list/list.view.ts
+++ reports/carriage/list/list.view.ts
@@ -1,5 +1,5 @@
 namespace $.$$ {
-	export class $mpk_tss_reports_carriages extends $.$mpk_tss_reports_carriages {
+	export class $mpk_tss_reports_carriage_list extends $.$mpk_tss_reports_carriage_list {
 		report(): $mpk_tss_reports_domain_report {
 			throw new $mpk_tss_todo
 		}
