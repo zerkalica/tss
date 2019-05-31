32451edeec10090a280fe5498ac4afbbd226c926
Stefan Zerkalica <zerkalica@gmail.com>
2019-05-31 14:44:34 +0300

    feat: zoom plot draft

--- reports/axle/chart/chart.view.ts
+++ reports/axle/chart/chart.view.ts
@@ -43,4 +43,49 @@ namespace $.$$ {
 			return '' + this.ruler_data()[Number(id)][0]
 		}
 	}
+
+	export class $mpk_tss_reports_axle_chart_pane extends $.$mpk_tss_reports_axle_chart_pane {
+		@$mol_mem
+		zoom_x(next?: number) {
+			const scale = super.scale()
+			if (next === undefined) {
+				return scale[0]
+			}
+
+			if (next < scale[0]) return scale[0]
+			if (next > 20) return 20
+
+			return next
+		}
+
+		@$mol_mem
+		scale() {
+			const scale = super.scale()
+			const zoom = this.zoom_x()
+
+			return [
+				zoom,
+				scale[1],
+			]
+		}
+
+		@$mol_mem
+		pan(next?: [number, number]): [number, number] {
+			const shift = super.shift() as [number, number]
+			if (next === undefined) return shift
+			let shift_x = next[0]
+			if (shift_x > this.gap_left()) shift_x = this.gap_left()
+			// const size = this.size_real()
+			// const scaled_x = Math.round(shift_x / this.scale()[0])
+			// if (shift_x > 0) shift_x = 0
+			return [
+				shift_x,
+				shift[1],
+			]
+		}
+
+		shift() {
+			return this.pan()
+		}
+	}
 }
